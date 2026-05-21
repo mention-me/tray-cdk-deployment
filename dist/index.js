078c78062050735ccd8465c87c36f9d4ef0e36bd
Gabriel Breda <gabriel.breda@mention-me.com>
2026-05-21 16:55:41 +0200

    chore: rebundle dist/ after dependency updates

--- dist/index.js
+++ dist/index.js
@@ -4143,7 +4143,7 @@ function save(namespaces) {
 function load() {
 	let r;
 	try {
-		r = exports.storage.getItem('debug');
+		r = exports.storage.getItem('debug') || exports.storage.getItem('DEBUG') ;
 	} catch (error) {
 		// Swallow
 		// XXX (@Qix-) should we be logging these?
@@ -4371,7 +4371,7 @@ function setup(env) {
 
 		const split = (typeof namespaces === 'string' ? namespaces : '')
 			.trim()
-			.replace(' ', ',')
+			.replace(/\s+/g, ',')
 			.split(',')
 			.filter(Boolean);
 
